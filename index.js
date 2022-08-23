115f39b0edeb73efe0e48c0e86963b57b877de27
Ian Almeida <iangtaonline1@gmail.com>
2022-08-23 10:55:04 -0300

    criando o index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+//Posição      0   1   2   3   4   5
+const notas = [10, 5, 8.5, 9, 6.5, 9]
+
+let media = (notas[0] + notas[1] + notas[2] + notas[3] + notas[4] + notas[5]) / notas.length
+
+console.log(media)
