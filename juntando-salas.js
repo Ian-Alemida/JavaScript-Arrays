67efd4651ea85ac9928bb2092afb97a0623c7aba
Ian Almeida <iangtaonline1@gmail.com>
2022-08-24 14:21:11 -0300

    juntando arrays com o comando .concat

---
+++ juntando-salas.js
@@ -0,0 +1,8 @@
+const salaDePhyton = ['Roger', 'Bruno', 'Lucas'] 
+const salaDeJavaScript = ['Julia', 'Rebeca', 'Farolesa']
+
+const salasUnificadas = salaDePhyton.concat(salaDeJavaScript)
+
+console.log(salaDePhyton)
+console.log(salaDeJavaScript)
+console.log(salasUnificadas)
