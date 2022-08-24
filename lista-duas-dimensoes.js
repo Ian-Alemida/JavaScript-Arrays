9a07ea3cbfc7dc2a5af92ba96d8008fdccbfff5a
Ian Almeida <iangtaonline1@gmail.com>
2022-08-24 15:15:11 -0300

    Manipulando arrays de duas dimensões

---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,14 @@
+const alunos = ['Laika', 'Juão', 'Pamela', 'Jhonson']
+const notasAlunos = ['10', '4', '7.5', '8']
+
+const alunosMaisNotas= [alunos, notasAlunos]
+
+//console.log(`${alunosMaisNotas[0][2]}, sua nota é ${alunosMaisNotas[1][2]}`)
+
+nomes = ["Ana", "Juliana", "Leonardo"]
+idades = [30, 35, 28]
+faculdade = [false, true, true]
+
+funcionarios = [nomes,idades,faculdade]
+
+console.log(`nome: ${funcionarios[0][0]} idade:${funcionarios[1][0]} faculdade: ${funcionarios[2][0]}`)
