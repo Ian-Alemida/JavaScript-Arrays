c6fadfe8c6f5dc70f1e5bbf8507daefab851a267
Ian Almeida <iangtaonline1@gmail.com>
2022-08-30 10:46:53 -0300

    Media geral da sala com .reduce

---
+++ media-geral.js
@@ -0,0 +1,12 @@
+const salaJs = [7, 8, 8, 7, 10, 6.5, 4, 10, 7]
+const salaJava = [6, 5, 8, 9, 5, 6]
+const salaPython = [7, 3.5, 8, 9.5]
+
+function mediaSala(notasDaSala){
+    const somaDasNotas = notasDaSala.reduce((acum, soma) => soma + acum,0)
+    return somaDasNotas/notasDaSala.length  
+}
+
+console.log(`A media da sala de JavaScript é: ${mediaSala(salaJs)}`)
+console.log(`A media da sala de Java é: ${mediaSala(salaJava)}`)
+console.log(`A media da sala de Python é: ${mediaSala(salaPython)}`)
